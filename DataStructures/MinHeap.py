efc11f23c85a68a23e8fbc2f139a930ad9353f35
jake6724 <jakealdridge6724@gmail.com>
2025-05-07 11:22:15 -0400

    Continue work fixing heapify

--- DataStructures/MinHeap.py
+++ DataStructures/MinHeap.py
@@ -4,38 +4,34 @@ find parent: arr[(i - 1) // 2]
 find left child: (i * 2) + 1
 find right child: (i * 2) + 2 
 """
+
+# HEAPYIFY AND BUILD HEAP ARE STILL BROKEN.
+
 class minHeap():
 	def __init__(self, a=[]):
 		self.heap = a
 		self.heap_size = len(self.heap) - 1
 
-		self.build_min_heap()
-
 	def __str__(self):
 		return str(self.heap)
 
-	def build_min_heap(self):
-		for i in range(((self.heap_size // 2) - 1), 0, -1):
-			print(i)
-			self.min_heapify(i)
+	# def min_heapify(self, index): 
+	# 	"""Create a heap from an unsorted array. Goal is to maintain the heap invariant. \n
+	# 	Index represents the current parent node"""
+	# 	smallest = index
+	# 	child_left_index = self.get_child_left(index)
+	# 	child_right_index = self.get_child_right(index)
 
-	def min_heapify(self, index): 
-		"""Create a heap from an unsorted array. Goal is to maintain the heap invariant. \n
-		Index represents the current parent node"""
-		smallest = index
-		child_left_index = self.get_child_left(index)
-		child_right_index = self.get_child_right(index)
+	# 	# Compare for each child, but only check that child if its index is inbounds (if not it doesn't really exist)
+	# 	if (child_left_index < self.heap_size) and (self.heap[child_left_index] < smallest):
+	# 		smallest = child_left_index
 
-		# Compare for each child, but only check that child if its index is inbounds (if not it doesn't really exist)
-		if (child_left_index < self.heap_size) and (self.heap[child_left_index] < smallest):
-			smallest = child_left_index
-
-		elif (child_right_index < self.heap_size) and (self.heap[child_right_index] < smallest):
-			smallest = child_right_index
+	# 	elif (child_right_index < self.heap_size) and (self.heap[child_right_index] < smallest):
+	# 		smallest = child_right_index
 		
-		if smallest != index: # This means that a child has a smaller value and parent should move down
-			self.heap[index], self.heap[smallest] = self.heap[smallest], self.heap[index]
-			self.min_heapify(smallest) # Recurse up to where the smallest was just moved and rerun
+	# 	if smallest != index: # This means that a child has a smaller value and parent should move down
+	# 		self.heap[index], self.heap[smallest] = self.heap[smallest], self.heap[index]
+	# 		self.min_heapify(smallest) # Recurse up to where the smallest was just moved and rerun
 
 	def pop(self):
 		# Return None if heap is empty
@@ -97,6 +93,33 @@ class minHeap():
 		"""Return the index of the right child of node at passed index"""
 		return (index * 2) + 2
 	
+# These 2 functions exist outside of the Heap class, and are used to heapify an unsorted, non-heap array
+def min_heapify(arr, heap_size, index):
+		"""Create a heap from an unsorted array. Goal is to maintain the heap invariant. \n
+		Index represents the current parent node"""
+		smallest = index
+		child_left_index = (index * 2) + 1
+		child_right_index = (index * 2) + 2
+
+		# Compare for each child, but only check that child if its index is inbounds (if not it doesn't really exist)
+		if child_left_index < heap_size and arr[child_left_index] < arr[smallest]:
+			smallest = child_left_index
+
+		if (child_right_index < heap_size) and (arr[child_right_index] < arr[smallest]):
+			smallest = child_right_index
+		
+		if smallest != index: # This means that a child has a smaller value and parent should move down
+			arr[index], arr[smallest] = arr[smallest], arr[index]
+			min_heapify(arr, heap_size, smallest) # Recurse up to where the smallest was just moved and rerun
+
+def build_min_heap(arr):
+	#               *
+	# [4, 7, 32, 8, 0, 5, 2, 5, 8, -1]
+	start_index = (len(arr) // 2) - 1 # 4
+	for i in range(start_index, -1, -1):
+		print("i:", i)
+		min_heapify(arr, len(arr), i)
+
 # Runner code
 def main():
 
@@ -133,5 +156,11 @@ def main():
 	print("Another pop!", min_heap)
 	min_heap.pop()
 	print("Another pop!", min_heap)
+
+	print("4. Test build heapify/heapify ")
+	a = [4,7,32,8,0,5,2,5,8,-1]
+	print("Starting arr: ", a)
+	build_min_heap(a)
+	print("After build_min_heap(a): ", a)
 
 main()
